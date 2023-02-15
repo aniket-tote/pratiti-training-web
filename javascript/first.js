ba24449ec8a60041b15cf7413d2e284db75b116b
aniket-tote <anikettote2001@gmail.com>
2023-02-15 18:49:09 +0530

    3rd day

---
+++ javascript/first.js
@@ -0,0 +1,69 @@
+// for (let i = 0; i < 10; i++) {
+//   console.log("hii");
+// }
+
+// const student = {
+//   name: "aniket",
+//   rollNo: 3,
+//   place: "hingoli",
+// };
+
+// let { name, rollNo, place } = student;
+
+// console.log(name + " " + rollNo + " " + place);
+
+// const arr1 = [1, 2, 3, 4, 5];
+
+// const [a, b, c] = arr1;
+
+// console.log(a + " " + b + " " + c);
+
+// function sayMyName() {
+//   for (let i = 0; i < 10; i++) {
+//   }
+//   console.log(i);
+// }
+
+// sayMyName();
+
+// (function () {
+//   console.log("hello");
+// })();
+
+// (() => {
+//   console.log("hello");
+// })();
+
+// const a = 5;
+
+// function temp() {
+//   const a = 10;
+//   console.log(a);
+// }
+
+// temp();
+
+// console.log(a);
+
+// function notUsingClosure() {
+//   let counter = 0;
+//   function plus() {
+//     counter += 1;
+//   }
+//   plus();
+//   return counter;
+// }
+
+// console.log(notUsingClosure());
+
+const add = (function () {
+  let counter = 0;
+  return function () {
+    counter += 1;
+    return counter;
+  };
+})();
+
+console.log(add());
+console.log(add());
+console.log(add());
